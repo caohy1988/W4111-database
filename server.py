7da026bd1853af71ae433853bb6fa56fec482e2c
Ruoqi77Wang <rw2612@columbia.edu>
2016-03-30 21:12:17 -0400

    Added files via upload

--- server.py
+++ server.py
@@ -19,13 +19,13 @@ import os
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
 from flask import Flask, request, render_template, g, redirect, Response
- 
+
 import re
-import time 
-import json 
+import time
+import json
 import md5
 import pdb
-import random 
+import random
 import psycopg2
 import traceback
 
@@ -38,7 +38,7 @@ app = Flask(__name__, template_folder=tmpl_dir)
 # However for the project you will need to connect to your Part 2 database in order to use the
 # data
 #
-# XXX: The URI should be in the format of: 
+# XXX: The URI should be in the format of:
 #
 #     postgresql://USER:PASSWORD@w4111db.eastus.cloudapp.azure.com/username
 #
@@ -61,14 +61,14 @@ engine = create_engine(DATABASEURI)
 #
 # after these statements run, you should see a file test.db in your webserver/ directory
 # this is a sqlite database that you can query like psql typing in the shell command line:
-# 
+#
 #     sqlite3 test.db
 #
 # The following sqlite3 commands may be useful:
-# 
+#
 #     .tables               -- will list the tables in the database
 #     .schema <tablename>   -- print CREATE TABLE statement for table
-# 
+#
 # The setup code should be deleted once you switch to using the Part 2 postgresql database
 #
 #engine.execute("""DROP TABLE IF EXISTS test;""")
@@ -86,7 +86,7 @@ engine = create_engine(DATABASEURI)
 @app.before_request
 def before_request():
   """
-  This function is run at the beginning of every web request 
+  This function is run at the beginning of every web request
   (every time you enter an address in the web browser).
   We use it to setup a database connection that can be used throughout the request
 
@@ -120,7 +120,7 @@ def teardown_request(exception):
 #       @app.route("/foobar/", methods=["POST", "GET"])
 #
 # PROTIP: (the trailing / in the path is important)
-# 
+#
 # see for routing: http://flask.pocoo.org/docs/0.10/quickstart/#routing
 # see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
 #
@@ -146,7 +146,7 @@ def index():
     #print item['rid']
   #cursor.close()
   names = []
-  
+
   #
   # Flask uses Jinja templates, which is an extension to HTML where you can
   # pass data to a template and dynamically generate HTML based on the data
@@ -156,14 +156,14 @@ def index():
   # You can see an example template in templates/index.html
   #
   # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
+  # for example, "data" key in the context variable defined below will be
   # accessible as a variable in index.html:
   #
   #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
   #     <div>{{data}}</div>
-  #     
+  #
   #     # creates a <div> tag for each element in data
-  #     # will print: 
+  #     # will print:
   #     #
   #     #   <div>grace hopper</div>
   #     #   <div>alan turing</div>
@@ -190,14 +190,14 @@ def index():
   # You can see an example template in templates/index.html
   #
   # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
+  # for example, "data" key in the context variable defined below will be
   # accessible as a variable in index.html:
   #
   #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
   #     <div>{{data}}</div>
-  #     
+  #
   #     # creates a <div> tag for each element in data
-  #     # will print: 
+  #     # will print:
   #     #
   #     #   <div>grace hopper</div>
   #     #   <div>alan turing</div>
@@ -220,7 +220,7 @@ def approot():
 
 #
 # This is an example of a different path.  You can see it at
-# 
+#
 #     localhost:8111/another
 #
 # notice that the functio name is another() rather than index()
@@ -248,22 +248,60 @@ def back():
 
 @app.route('/search', methods=['POST','GET'])
 def search():
-  for item in request.form:
-    print item
-    print request.form[item]
-  restaurant = request.form['restaurant']
-  category = request.form['category']
-  calorie_upper = request.form['calorie_upper']
-  calorie_lower = request.form['calorie_lower']
-  sodium_upper = request.form['sodium_upper']
-  sodium_lower = request.form['sodium_lower']
-  protein_upper = request.form['protein_upper']
-  protein_lower = request.form['protein_lower']
-  keyword = request.form['keyword']
+  restaurant = '%'
+  category = '%'
+  calorie_upper = 100000
+  calorie_lower = 0
+  sodium_upper = 100000
+  sodium_lower = 0
+  protein_upper = 100000
+  protein_lower = 0
+  keyword = '%'
+  #for item in request.form:
+  #  print item
+  #  print request.form[item]
+  temp = request.form['restaurant']
+  print len(temp)
+  if temp != '':
+    seq = ('%', temp, '%')
+    restaurant = "".join(seq)
+    print restaurant
+  #restaurant = request.form['restaurant']
+  temp = request.form['category']
+  if temp != '':
+    seq = ('%', temp, '%')
+    category = "".join(seq)
+    print category
+  #category = request.form['category']
+  temp = request.form['calorie_upper']
+  if temp != '':
+    calorie_upper = temp
+  temp = request.form['calorie_lower']
+  if temp != '':
+    calorie_lower = temp
+  temp = request.form['sodium_upper']
+  if temp != '':
+    sodium_upper = temp
+  #temp = request.form['sodium_lower']
+  temp = request.form['sodium_lower']
+  if temp != '':
+    sodium_lower = temp
+  temp = request.form['protein_upper']
+  if temp != '':
+    protein_upper = temp
+  temp = request.form['protein_lower']
+  if temp != '':
+    protein_lower = temp
+  temp  = request.form['keyword']
+  if temp != '':
+    seq = ('%', temp, '%')
+    keyword = "".join(seq)
+    print keyword
+  #keyword = request.form['keyword']
 
-  q = 'select rm.namemenu, rt.name,m.caloryamount, m.proteinamount, m.sodiumamount, rm.price from restaurants rt, restaurantsmenu rm, menunutrients m where rm.rid = rt.rid and rm.mid = m.mid and m.rid = rm.rid and rt.name = %s and rm.namemenu = %s and rm.category = %s and  m.caloryamount < %s and m.caloryamount > %s and m.sodiumamount < %s and m.sodiumamount > %s and m.proteinamount < %s and m.proteinamount > %s;'
+  q = 'select rm.namemenu, rt.name,m.caloryamount, m.proteinamount, m.sodiumamount, rm.price from restaurants rt, restaurantsmenu rm, menunutrients m where rm.rid = rt.rid and rm.mid = m.mid and m.rid = rm.rid and rt.name like %s and lower(rm.namemenu) like %s and rm.category like %s and  m.caloryamount < %s and m.caloryamount > %s and m.sodiumamount < %s and m.sodiumamount > %s and m.proteinamount < %s and m.proteinamount > %s;'
   #q = "select rm.namemenu, rt.name,m.caloryamount, m.proteinamount, m.sodiumamount, rm.price from restaurants rt, restaurantsmenu rm, menunutrients m where rm.rid = rt.rid and rm.mid = m.mid and m.rid = rm.rid; "
-  print q  
+  print q
   try:
     cursor = g.conn.execute(q, (restaurant, keyword, category, calorie_upper, calorie_lower,sodium_upper, sodium_lower, protein_upper, protein_lower,))
   #cursor = g.conn.execute(q)
@@ -279,7 +317,7 @@ def search():
   #
   # example of a database query
   #
-  
+
     for result in cursor:
       print result['namemenu']
       foodname.append(result['namemenu'])# can also be accessed using result[0]
@@ -294,10 +332,10 @@ def search():
       print result['price']
       price.append(result['price'])
     cursor.close()
-
+    print restaurantname
   #context = dict(foodname = foodname, restautrant = restaurantname, sodium = sodium, calorie = calorie, protein = protein, price = price )
     #g.conn.execute('INSERT INTO test VALUES (NULL, ?)', name)
-    return render_template("index.html", foodname = foodname, restautrant = restaurantname, sodium = sodium, calorie = calorie, protein = protein, price = price)
+    return render_template("index.html", foodname = foodname, restaurant = restaurantname, sodium = sodium, calorie = calorie, protein = protein, price = price)
   except Exception as e:
     q = "Not found"
     print q
@@ -336,64 +374,67 @@ def order():
   foodname_list.append(foodname)
   restaurant_list.append(restaurant)
   name_list.append(name)
-  address_list.append(address) 
+  address_list.append(address)
   amount.append(quantity)
-  q = 'select rm.mid, rt.rid, rm.price, m.caloryamount, m.sodiumamount, m.proteinamount from restaurants rt, restaurantsmenu rm, menunutrients m where rt.rid = rm.rid and m.mid = rm.mid and rm.namemenu = %s and rt.name = %s;' 
-  cursor1 = g.conn.execute(q, (foodname, restaurant, ) )
+  q = 'select rm.mid, rt.rid, rm.price, m.caloryamount, m.sodiumamount, m.proteinamount from restaurants rt, restaurantsmenu rm, menunutrients m where rt.rid = rm.rid and m.mid = rm.mid and rm.namemenu = %s and rt.name = %s;'
   print q
-  for  result in cursor1:
-    mid.append(result['mid'])
-    print result['mid']
-    rid.append(result['rid'])
-    print result['rid']
-    price = price + int(result['price'])
-    print result['price']
-    calorie = calorie + int(result['caloryamount'])
-    print result['caloryamount']
-    sodium = sodium + int(result['sodiumamount'])   
-    print result['sodiumamount']
-    protein = protein + int(result['proteinamount'])
-    print result['proteinamount']
-  cursor1.close()
-  q2 = 'select uid from usersearch;'
-  print q2
-  cursor2 = g.conn.execute(q2)
-  for result in cursor2:
-    uid.append(result['uid'])
-    print result['uid']
-  length_uid = len(uid) 
-  cursor2.close()
-  uid_new = length_uid + 21
-  q3 = 'select oid from userorder;'
-  cursor3 = g.conn.execute(q3)
-  for result in cursor3:
-    oid.append(result['oid'])  
-    print result['oid']
-  cursor3.close()
-  length_oid = len(oid)   
-  oid_new = length_oid + 100
+  try:
+    cursor1 = g.conn.execute(q, (foodname, restaurant, ) )
+    for  result in cursor1:
+      mid.append(result['mid'])
+      print result['mid']
+      rid.append(result['rid'])
+      print result['rid']
+      price = price + int(result['price'])
+      print result['price']
+      calorie = calorie + int(result['caloryamount'])
+      print result['caloryamount']
+      sodium = sodium + int(result['sodiumamount'])
+      print result['sodiumamount']
+      protein = protein + int(result['proteinamount'])
+      print result['proteinamount']
+    cursor1.close()
+    q2 = 'select uid from usersearch;'
+    print q2
+    cursor2 = g.conn.execute(q2)
+    for result in cursor2:
+      uid.append(result['uid'])
+      print result['uid']
+    length_uid = len(uid)
+    cursor2.close()
+    uid_new = length_uid + 21
+    q3 = 'select oid from userorder;'
+    cursor3 = g.conn.execute(q3)
+    for result in cursor3:
+      oid.append(result['oid'])
+      print result['oid']
+    cursor3.close()
+    length_oid = len(oid)
+    oid_new = length_oid + 100
 
-  q4 = 'insert into usersearch (uid, name) Values(%s, %s)'
-  print q4
-  g.conn.execute(q4, (uid_new, name,))
-  totalprice = price * int(amount[0])
-  print totalprice
-  totalprice_list.append(totalprice)
-  q5 = 'insert into userorder (uid, oid, deliveraddress, totalprice) Values(%s, %s, %s, %s)'
-  print q5
-  print address
-  print totalprice
-  g.conn.execute(q5, (uid_new, oid_new, address, totalprice,))
-  print "success"
-  print foodname_list
-  print name_list
-  print address_list
-  print amount
-  print restaurant_list
-  print totalprice_list
+    q4 = 'insert into usersearch (uid, name) Values(%s, %s)'
+    print q4
+    g.conn.execute(q4, (uid_new, name,))
+    totalprice = price * int(amount[0])
+    print totalprice
+    totalprice_list.append(totalprice)
+    q5 = 'insert into userorder (uid, oid, deliveraddress, totalprice) Values(%s, %s, %s, %s)'
+    print q5
+    print address
+    print totalprice
+    g.conn.execute(q5, (uid_new, oid_new, address, totalprice,))
+    print "success"
+    print foodname_list
+    print name_list
+    print address_list
+    print amount
+    print restaurant_list
+    print totalprice_list
 
-  return render_template("final.html", foodname = foodname_list, name = name_list, deliveraddress = address_list, quantity = amount, restaurant = restaurant_list, totalprice = totalprice_list)
+    return render_template("final.html", foodname = foodname_list, name = name_list, deliveraddress = address_list, quantity = amount, restaurant = restaurant_list, totalprice = totalprice_list)
 #     return redictrict('another')
+  except Exception as e:
+    return render_template("index.html")
 
 
 
